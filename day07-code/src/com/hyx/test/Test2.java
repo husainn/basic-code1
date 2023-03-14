be4213f97b741908e064a0f8f82a5fa7210cf3f9
huge <975810720@qq.com>
2023-03-14 22:40:51 +0800

    git

--- day07-code/src/com/hyx/test/Test2.java
+++ day07-code/src/com/hyx/test/Test2.java
@@ -2,6 +2,7 @@ package com.hyx.test;
 
 public class Test2 {
     public static void main(String[] args) {
-        //判断101~200之间有多少个素数，并除数所有素数
+        //判断101~200之间有多少个素数，并除数所有素数,git测试
+
     }
 }
