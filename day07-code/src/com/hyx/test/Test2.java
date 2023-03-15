723e69b18b5b04bbd94c770b9f16592972def144
huge <975810720@qq.com>
2023-03-15 23:23:37 +0800

    lianxi3

--- day07-code/src/com/hyx/test/Test2.java
+++ day07-code/src/com/hyx/test/Test2.java
@@ -2,7 +2,21 @@ package com.hyx.test;
 
 public class Test2 {
     public static void main(String[] args) {
-        //判断101~200之间有多少个素数，并除数所有素数,git测试
-
+        //判断101~200之间有多少个质数，并打印所有质数
+        int count = 0;
+        for (int i = 101; i <=200 ; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println("当前数字"+i+"是质数");
+                count++;
+            }
+        }
+        System.out.println("总共有"+count+"个质数");
     }
 }
---
+++ day07-code/src/com/hyx/test/Test3.java
@@ -0,0 +1,16 @@
+package com.hyx.test;
+
+import java.util.Random;
+
+public class Test3 {
+    public static void main(String[] args) {
+/*        需求：
+            定义方法实现随机产生一个5位的验证码
+            验证码格式：
+            长度为5
+            前四位是大写字母或者小写字母
+            最后一位是数字
+        */
+        
+    }
+}
